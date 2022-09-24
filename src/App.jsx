c7bd4611596d937cfacf791e5dcfa8107eeb74f5
bryanbanguera146@gmail.com <bryanbanguera146@gmail.com>
2022-09-24 05:14:03 -0500

    maquetando y dando estilos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
+import Loading from './components/Loading'
 import WeatherCard from './components/WeatherCard'
 
 function App() {
 
 const [coords, setCoords] = useState()
 const [weather, setWeather] = useState()
+const [temperature, setTemperature] = useState()
 
 useEffect(() => {
   const success = (pos) => {
@@ -21,14 +23,21 @@ useEffect(() => {
 
 console.log(coords)
 
-// forma para hacer peticion a un api de navegador
+// forma para hacer peticion a un api de navegador(peticion de clima)
 
 useEffect (() => {
   if (coords) {
     const APIKEY = 'bd2da00459eaab2abbdeec6c1cea994a'
     const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
     axios.get(URL)
-      .then(res => setWeather(res.data))
+      .then(res => {
+        const celsius = (res.data.main.temp -273.15).toFixed(1)
+        const farenheit = (celsius * 9 / 5 + 32).toFixed(1)
+        setTemperature({celsius,farenheit})
+        setWeather(res.data)
+      })
+
+
       .catch(err => console.log(err))
   }
 }, [coords])
@@ -36,9 +45,13 @@ useEffect (() => {
 
   return (
     <div className="App">
-      <WeatherCard weather={weather} />
+      {
+      weather ?
+      <WeatherCard weather={weather} temperature={temperature} />
+      :
+      < Loading />
+      }
     </div>
-      
   )
 }
 
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,30 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const WeatherCard = ({weather}) => {
+const WeatherCard = ({ weather, temperature }) => {
+    const [isCelsius, setIsCelsius] = useState(true);
+    const changeTemperature = () => setIsCelsius(!isCelsius);
 
-    console.log(weather)
+    console.log(weather);
 
-    return (
-    <article className='card'>
-        <h1>weather App ⛈🌦🌥🌤🌨</h1>
-        <h2>{`${weather?.name}, "${weather?.sys.country}"`}</h2>
-        <section>
-            <img src={weather ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`:""} alt="" />
-            
-        </section>
-        <section>
-            <h3>"{weather?.weather[0].description}"</h3>
-            <ul>
-                <li><strong>wind Speed</strong>{weather?.wind.speed}m/s</li>
-                <li><strong>Clouds</strong>{weather?.clouds.all}%</li>
-                <li><strong>Presure</strong>{weather?.main.pressure}hPa</li>
+return (
+    <article className="card">
+        <h1 className="card__title">Weather App</h1>
+        <h2 className="card__subtitle">{`${weather?.name}, ${weather?.sys.country}`}</h2>
+            <section className="card__first-section">
+                <img
+                    className="card__icon"
+                    src={
+                        weather
+                            ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`
+                            : ""
+                    }
+                    alt="image"
+                />
+            </section>
+
+    <section className="card__second-section">
+        <h3 className="second__title">"{weather?.weather[0].description}"</h3>
+            <ul className="second__list">
+                <li className="second__item">
+                    <span>wind Speed:</span>
+                    {weather?.wind.speed}m/s
+                </li>
+                <li className="second__item">
+                    <span>Clouds:</span>
+                    {weather?.clouds.all}%
+                </li>
+                <li className="second__item">
+                    <span>Presure:</span>
+                    {weather?.main.pressure}hPa
+                </li>
             </ul>
-        </section>
+    </section>
 
-        <h2>Tenperatura:</h2>
-        <button></button>
+    <h2 className="card__temperature">
+        {isCelsius
+        ? `${temperature?.celsius} °C`
+        : `${temperature?.farenheit} °F`
+        }
+    </h2>
+
+    <button className="card__btn" onClick={changeTemperature}>
+        {isCelsius ? "Change to °F" : "Chage to °C"}
+    </button>
     </article>
-    )
-}
+    );
+};
 
-export default WeatherCard
+export default WeatherCard;
