1c115a2a1b68a6221310be3fc311146d33481c25
bryanbanguera146@gmail.com <bryanbanguera146@gmail.com>
2022-09-23 22:30:51 -0500

    implementando logica .JS

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -8,6 +8,21 @@ const WeatherCard = ({weather}) => {
     <article className='card'>
         <h1>weather App ⛈🌦🌥🌤🌨</h1>
         <h2>{`${weather?.name}, "${weather?.sys.country}"`}</h2>
+        <section>
+            <img src={weather ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`:""} alt="" />
+            
+        </section>
+        <section>
+            <h3>"{weather?.weather[0].description}"</h3>
+            <ul>
+                <li><strong>wind Speed</strong>{weather?.wind.speed}m/s</li>
+                <li><strong>Clouds</strong>{weather?.clouds.all}%</li>
+                <li><strong>Presure</strong>{weather?.main.pressure}hPa</li>
+            </ul>
+        </section>
+
+        <h2>Tenperatura:</h2>
+        <button></button>
     </article>
     )
 }
